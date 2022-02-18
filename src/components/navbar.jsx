90a3c161b048fdac1c196a7d6e5457958c837997
Muhammad Abdurrohman <abdurrohmanmuhammad123@gmail.com>
2022-02-18 14:08:08 +0700

    first commit

---
+++ src/components/navbar.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <div className="left">
+                <h1 className="text-left text-red-500">This is Navbar</h1>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
